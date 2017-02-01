f09dc7d9b8995420d2c456fbae520dabb12a7747
danielius <danielius55@yahoo.com>
2017-02-01 11:36:58 +0200

    first kata

---
+++ kata/SwitchItUp/first.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: danielius
+ * Date: 17.2.1
+ * Time: 09.02
+ */
+namespace SwitchItUp;
+
+class first
+{
+    public function switch_it_up($number)
+    {
+        switch ($number) {
+            case 0:
+                return "Zero";
+                break;
+            case 1:
+                return "One";
+                break;
+            case 2:
+                return "Two";
+                break;
+            case 3:
+                return "Three";
+                break;
+            case 4:
+                return "Four";
+                break;
+            case 5:
+                return "Five";
+                break;
+            case 6:
+                return "Six";
+                break;
+            case 7:
+                return "Seven";
+                break;
+            case 8:
+                return "Eight";
+                break;
+            case 9:
+                return "Nine";
+                break;
+            default:
+                return "NaN";
+                break;
+        }
+    }
+}
